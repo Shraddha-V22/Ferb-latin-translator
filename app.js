a138601606a2529eeb1133c267f98be2fd509a0d
Shraddha-V22 <shraddhav1935@gmail.com>
2022-03-01 23:12:07 +0530

    output fetch errorHandler

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
-var btnTranslate = document.querySelector("#translateBtn");
+var btnTranslate = document.querySelector("#translate-Btn");
+var textInput = document.querySelector("#textarea");
+var outputDiv = document.querySelector("#output");
 
-btnTranslate.addEventListener("click", function clickHandler() {
-    console.log('clicked!')
+var url = "https://api.funtranslations.com/translate/ferb-latin.jso";
+
+function serverURL(text) {
+    return url + "?" + "text=" + text
 };
+
+function errorHandler(error) {
+    console.log("error occured" + error)
+};
+
+function clickHandler() {
+    
+    var InputTxt= textInput.value;
+
+    fetch(serverURL(InputTxt))
+        .then(response => response.json())
+        .then(json => 
+            outputDiv.innerText=json.contents.translated)
+        .catch(errorHandler);
+};
+
+btnTranslate.addEventListener("click", clickHandler);
+
